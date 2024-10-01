2846050812c708a9a9b8cf10af9f7fabeaa85e38
intel <syedmiskinquadriquadri99217@gmail.com>
2024-10-01 04:25:03 -0700

    1st time code

---
+++ methodds.java
@@ -0,0 +1,10 @@
+public class methodds {
+public static void main(String[] args){
+multiply(11,12);
+multiply(15,15);
+}
+public static void multiply(int x, int y){
+int product= x*y;
+System.out.println("The product of x and y is"+ product);
+}
+}
