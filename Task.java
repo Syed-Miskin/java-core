ceb0cfbede36b3139abb4d66e0ee2b0dbd40a3c6
intel <syedmiskinquadriquadri99217@gmail.com>
2024-10-02 22:56:39 -0700

    2nd code

---
+++ Task.java
@@ -0,0 +1,42 @@
+public class Task{
+   public static void main(String[] args){
+byte age=12;
+byte days=7;
+byte weeksOfYears=54;
+byte hoursInADay=24;
+byte alphabetInEnglish=26;
+byte continentsInWorld=7;
+short salaryPerDay=500;
+short daysInAYear=365;
+short countriesInWorld=196;
+short sizeOfThisFile=242;
+short year=2024;
+short storageInDdisk=412;
+int myPhoneNumber=1411756322;
+int collegeFee=80970;
+int secInADay=86400;
+int totalIncomeOfFaculty=50000;
+int scholorShipAmount=65000;
+
+System.out.println(year);
+System.out.println(collegeFee);
+System.out.println(salaryPerDay);
+System.out.println(sizeOfThisFile);
+System.out.println(weeksOfYears);
+
+year=2025;
+System.out.println(year);
+
+collegeFee=100000;
+System.out.println(collegeFee);
+
+salaryPerDay=450;
+System.out.println(salaryPerDay);
+
+weeksOfYears=55;
+System.out.println(weeksOfYears);
+
+sizeOfThisFile=611;
+System.out.println(sizeOfThisFile);
+}
+}
